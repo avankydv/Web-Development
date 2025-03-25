783be8676b9a0265604de0c32700da6b6ff0623a
Aditya Yadav <adityaydv12890@gmail.com>
2025-03-25 18:47:35 +0530

    Update index.js

--- Cohort - 3 week-1/index.js
+++ Cohort - 3 week-1/index.js
@@ -343,3 +343,35 @@
 //   console.log("promise succeded");
 // }
 // p.then(callback)
+
+
+
+// class Promise2 {
+//   constructor(fn) {
+//     function afterDone() {
+//       this.resolve();
+//     }
+//     fn(afterDone)
+
+//   then(callback) {
+//     this.resolve = callback();
+//   }
+// }
+
+
+// function readTheFile(Resolve) {
+//   setTimeout(function) {
+//     console.log("callback based settimeout completed");
+//     resolve();
+//   }, 3000;
+// }
+
+// function setTimeoutPromisified() {
+//   return new Promise2(readTheFile)
+// }
+  
+// let p = setTimeoutPromisified();
+// function callback() {
+//   console.log("callback has been called");
+// }
+// p.then(callback);
